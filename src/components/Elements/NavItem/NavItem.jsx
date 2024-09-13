f633f12603e2b77e12e392dad6a9aa6fa82e6af0
myogaazwar <yogaazwar123@gmail.com>
2024-09-13 08:54:46 +0700

    create: nav item & add: scroll link

--- src/components/Elements/NavItem/NavItem.jsx
+++ src/components/Elements/NavItem/NavItem.jsx
@@ -1,27 +1,40 @@
-export const NavItem = ({ classname, href, children }) => {
+import { NavLink } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+
+export const NavItem = ({ to, children }) => {
   return (
     <li>
-      <a
-        href={href}
-        className={`${classname} py-2 px-3 rounded-2xl transition duration-200 ease-in-out hover:bg-blue-600 hover:text-whiteColor`}
+      <ScrollLink
+        to={to}
+        smooth={true}
+        duration={300}
+        spy={true}
+        offset={-100}
+        activeClass='bg-blue-700 text-whiteColor'
+        className={`cursor-pointer py-2 px-3 rounded-2xl transition duration-200 ease-in-out  hover:bg-blue-600 hover:text-whiteColor`}
       >
         {children}
-      </a>
+      </ScrollLink>
     </li>
   );
 };
 
-export const NavItemPhone = ({ href, children }) => {
+export const NavItemPhone = ({ to, children }) => {
   return (
     <li className='w-full'>
-      <a
-        href={href}
+      <ScrollLink
+        to={to}
+        smooth={true}
+        duration={300}
+        spy={true}
+        offset={-100}
+        activeClass='bg-blue-600'
         className={
-          'hover:bg-whiteColor hover:text-black poppins-regular text-white px-10 py-1 rounded-3xl transition duration-200 ease-in-out'
+          'cursor-pointer hover:bg-whiteColor hover:text-black poppins-regular text-white px-10 py-1 rounded-3xl transition duration-200 ease-in-out'
         }
       >
         {children}
-      </a>
+      </ScrollLink>
     </li>
   );
 };
