b5207378faaf4520f971ce26236b4f66b4d3d66f
myogaazwar <yogaazwar123@gmail.com>
2024-09-13 08:55:08 +0700

    create: footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import Section from '../Elements/Section/Section';
+
+export const Footer = () => {
+  return (
+    <div classname={' mt-10 w-full bg-blue-700'}>
+      <div className='bg-blue-700 h-12 flex items-center justify-center'>
+        <h1 className='text-whiteColor poppins-bold'>
+          Created by Muhammad Yoga Azwar
+        </h1>
+      </div>
+    </div>
+  );
+};
