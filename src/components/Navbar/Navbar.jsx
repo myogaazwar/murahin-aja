3c69a9c7c482afce716c18413d36ed64936c3914
myogaazwar <yogaazwar123@gmail.com>
2024-09-13 08:55:41 +0700

    fix: navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { FaBars } from 'react-icons/fa';
 import Button from '../Elements/Button/Button';
-import { Link, NavLink } from 'react-router-dom';
 import { NavItem, NavItemPhone } from '../Elements/NavItem/NavItem';
 
 export const Navbar = () => {
@@ -23,31 +22,16 @@ export const Navbar = () => {
   }, [isOpenNav]);
 
   return (
-    <nav className='w-full bg-whiteColor text-mainTextColor '>
+    <nav className='w-full fixed border-b-2 border-blue-200 bg-whiteColor text-mainTextColor transition duration-300 '>
       <div className='h-24 flex justify-between items-center px-4 mx-auto max-w-7xl relative '>
         <div className='text-3xl poppins-semibold bg-clip-text text-transparent bg-gradient-to-r from-black to-blue-500'>
           <a href='#'>Murahin Aja</a>
         </div>
 
         <ul className='flex gap-x-4 text-lg max-md:hidden poppins-regular'>
-          {/* {navLink.map((item) => (
-            <li key={item.name}>
-              <NavLink
-                to={item.location}
-                className={({ isActive }) =>
-                  isActive
-                    ? 'py-2 px-3 rounded-2xl transition duration-200 ease-in-out bg-blue-600 text-whiteColor'
-                    : 'py-2 px-3 rounded-2xl transition duration-200 ease-in-out hover:bg-blue-600 hover:text-whiteColor'
-                }
-              >
-                {item.name}
-              </NavLink>
-            </li>
-          ))} */}
-
-          <NavItem href={'#'}>Home</NavItem>
-          <NavItem href={'#products'}>Products</NavItem>
-          <NavItem href={'#aboutUs'}>About Us</NavItem>
+          <NavItem to={'home'}>Home</NavItem>
+          <NavItem to={'products'}>Products</NavItem>
+          <NavItem to={'about-us'}>About Us</NavItem>
         </ul>
 
         {/* Phone Device */}
@@ -63,24 +47,9 @@ export const Navbar = () => {
             <AiOutlineClose />
           </Button>
 
-          {/* {navLink.map((item) => (
-            <li key={item.name} className='w-full'>
-              <NavLink
-                to={item.location}
-                className={({ isActive }) =>
-                  isActive
-                    ? ' bg-whiteColor text-black  poppins-regular  px-10 py-1 rounded-3xl transition duration-200 ease-in-out '
-                    : 'hover:bg-whiteColor hover:text-black poppins-regular text-white px-10 py-1 rounded-3xl transition duration-200 ease-in-out'
-                }
-              >
-                {item.name}
-              </NavLink>
-            </li>
-          ))} */}
-
-          <NavItemPhone href={'#'}>Home</NavItemPhone>
-          <NavItemPhone href={'#products'}>Products</NavItemPhone>
-          <NavItemPhone href={'#'}>About Us</NavItemPhone>
+          <NavItemPhone to={'home'}>Home</NavItemPhone>
+          <NavItemPhone to={'products'}>Products</NavItemPhone>
+          <NavItemPhone to={'about-us'}>About Us</NavItemPhone>
 
           <div className='flex flex-col gap-y-5 px-5 text-lg absolute bottom-24 w-full'>
             <Button classname={'  bg-blue-600 text-white hover:bg-blue-700'}>
