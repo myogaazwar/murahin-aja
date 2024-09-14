2eafead2131e7952fc0ab72a96f28e17f8994c07
myogaazwar <yogaazwar123@gmail.com>
2024-09-14 20:43:11 +0700

    change: padding price card component

--- src/components/Elements/Card/Card.jsx
+++ src/components/Elements/Card/Card.jsx
@@ -18,7 +18,7 @@ export const Card = ({
       <img className='w-full' src={image} alt={titleProduct} />
 
       <div className='max-[425px]:flex-col  flex justify-between items-center mt-5 mb-2'>
-        <p className='max-md:text-sm max-sm:text-xs'>{price}</p>
+        <p className='max-md:text-sm max-sm:text-xs pl-2'>{price}</p>
         <Button
           classname={
             'bg-blue-600 max-xl:text-sm max-lg:text-xs max-lg:px-2  text-whiteColor hover:bg-blue-700 active:bg-blue-800'
