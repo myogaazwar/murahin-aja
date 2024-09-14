530055619914756fcef9dbbb7b5455b9d7e73a95
myogaazwar <yogaazwar123@gmail.com>
2024-09-14 13:31:18 +0700

    add: router register page

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import './App.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
+import RegisterPage from './pages/Register';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
   },
 ]);
 
