cd60d3df0d1e178e1fc4f64f7ab55380bf8ff2bc
myogaazwar <yogaazwar123@gmail.com>
2024-09-14 20:42:29 +0700

    create: usestate products & loading

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import { productData } from '../../utility/productData';
 import Button from '../Elements/Button/Button';
 import { Card } from '../Elements/Card/Card';
 import Section from '../Elements/Section/Section';
 import redShirt from '/image/shirt-red.png';
+import { getDataProducts } from '../../services/product.service';
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dataProducts = await getDataProducts();
+        setProducts(dataProducts);
+        setLoading(false);
+        console.log(dataProducts);
+      } catch (error) {
+        setLoading(false);
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Section id={'products'} classname={' max-sm:px-0 p-5 h-full'}>
@@ -15,17 +36,23 @@ const Products = () => {
             </h2>
           </div>
 
+          {loading && (
+            <div className='bg-whiteColor max-w-2xl mx-auto h-40 shadow-inner drop-shadow-md flex flex-col items-center justify-center rounded-3xl'>
+              <h1 className='poppins-bold-italic text-3xl'>Loading...</h1>
+            </div>
+          )}
+
           <div className=' grid grid-cols-4 gap-x-4 max-[863px]:grid-cols-3 max-[711px]:grid-cols-2 max-[345px]:grid-cols-1'>
-            {productData.map((item) => (
+            {products.map((item) => (
               <Card
-                key={item.name}
+                key={item.title}
                 classname={
-                  'mx-auto max-lg:w-48 max-[863px]:w-60 max-[807px]:w-52 max-[711px]:w-60 max-[557px]:w-44 max-[425px]:w-36 max-[345px]:w-56'
+                  ' mb-3 mx-auto max-lg:w-48 max-[863px]:w-60 max-[807px]:w-52 max-[711px]:w-60 max-[557px]:w-44 max-[425px]:w-36 max-[345px]:w-56'
                 }
-                titleProduct={item.name}
+                titleProduct={item.title}
                 titleCategory={item.category}
                 image={item.image}
-                price={item.price}
+                price={`$${item.price}`}
               />
             ))}
           </div>
