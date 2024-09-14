2877911beca6a0327e29b36eec61fc1896c7f75a
myogaazwar <yogaazwar123@gmail.com>
2024-09-14 19:13:05 +0700

    create: form register component and change: register page

---
+++ src/components/FormInputs/FormRegister.jsx
@@ -0,0 +1,48 @@
+import Button from '../Elements/Button/Button';
+import InputForm from '../Elements/Input/Index';
+
+const FormRegister = () => {
+  return (
+    <form action=''>
+      <InputForm
+        id={'fullname'}
+        name={'fullname'}
+        placeholder={'Enter your fullname'}
+        title={'Full Name'}
+        type={'text'}
+        key={'fullname'}
+      />
+
+      <InputForm
+        id={'email'}
+        name={'email'}
+        placeholder={'Enter your email address'}
+        title={'Email'}
+        type={'email'}
+        key={'email'}
+      />
+
+      <InputForm
+        id={'password'}
+        name={'password'}
+        placeholder={'********'}
+        title={'Password'}
+        type={'password'}
+        key={'password'}
+      />
+
+      <InputForm
+        id={'confirmPassword'}
+        name={'confirmPassword'}
+        placeholder={'Enter your confirm password'}
+        title={'Confirm Password'}
+        type={'password'}
+        key={'confirmPassword'}
+      />
+
+      <Button classname={'w-full mt-10 bg-blue-600 text-white'}>Sign Up</Button>
+    </form>
+  );
+};
+
+export default FormRegister;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,91 +1,18 @@
-import { NavLink } from 'react-router-dom';
-import Button from '../components/Elements/Button/Button';
 import bgRegister from '/image/bg-register.jpg';
 
-import { GoArrowLeft } from 'react-icons/go';
+import AuthLayouts from '../components/Layouts/AuthLayouts';
+import FormRegister from '../components/FormInputs/FormRegister';
 
 const RegisterPage = () => {
   return (
-    <section className='h-screen w-full flex justify-center items-center '>
-      <div className='hidden md:block md:h-screen'>
-        <img className=' w=full h-full' src={bgRegister} alt='' />
-      </div>
-
-      <div className='px-5 mx-auto flex flex-col justify-center md:shadow-xl md:p-8 md:rounded-md md:w-[600px] md:px-10'>
-        <h1 className='poppins-medium text-2xl my-2 md:text-3xl'>Register</h1>
-        <p className='poppins-light text-xs'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic tempore
-          maxime at voluptate veniam provident laboriosam eos vel soluta atque.
-        </p>
-
-        <div className='flex flex-col mt-5 poppins-regular gap-y-2'>
-          <label className='text-sm' htmlFor='name'>
-            Full name
-          </label>
-          <input
-            id='name'
-            type='text'
-            placeholder='Enter your name'
-            className='border outline-none  focus:border-blue-600 placeholder:text-sm text-sm text-slate-500 px-2 py-2'
-          />
-        </div>
-
-        <div className='flex flex-col mt-5 poppins-regular gap-y-2'>
-          <label className='text-sm' htmlFor='email'>
-            Email
-          </label>
-          <input
-            id='email'
-            type='email'
-            placeholder='example@gmail.com'
-            className='border outline-none  focus:border-blue-600 placeholder:text-sm text-sm text-slate-500 px-2 py-2'
-          />
-        </div>
-
-        <div className='flex flex-col mt-5 poppins-regular gap-y-2'>
-          <label className='text-sm' htmlFor='password'>
-            Password
-          </label>
-          <input
-            id='password'
-            type='password'
-            placeholder='**********'
-            className='border outline-none  focus:border-blue-600 placeholder:text-sm text-sm text-slate-500 px-2 py-2'
-          />
-        </div>
-
-        <div className='flex flex-col mt-5 poppins-regular gap-y-2'>
-          <label className='text-sm' htmlFor='confirm-password'>
-            Confirm Password
-          </label>
-          <input
-            id='confirm-password'
-            type='password'
-            placeholder='Confirm your password'
-            className='border outline-none  focus:border-blue-600 placeholder:text-sm text-sm text-slate-500 px-2 py-2'
-          />
-        </div>
-
-        <Button classname={'w-full mt-10 bg-blue-600 text-white'}>
-          Sign Up
-        </Button>
-
-        <div className='text-sm mt-5 text-center flex flex-col gap-y-2'>
-          <p>
-            Already have an account?{' '}
-            <NavLink className='text-blue-600 underline underline-offset-4 hover:text-blue-800'>
-              Sign in.
-            </NavLink>
-          </p>
-
-          <div className='flex items-center justify-center hover:text-blue-700 '>
-            <GoArrowLeft />
-
-            <NavLink to={'/'}>Back to home</NavLink>
-          </div>
-        </div>
-      </div>
-    </section>
+    <AuthLayouts
+      type={'register'}
+      title={'Register Page'}
+      image={bgRegister}
+      description={'Join With US'}
+    >
+      <FormRegister />
+    </AuthLayouts>
   );
 };
 
