1b79a22714fb8f8be691453554f5d7a0eef179e7
myogaazwar <yogaazwar123@gmail.com>
2024-09-22 19:16:09 +0700

    add: props type

--- src/components/Elements/Button/Button.jsx
+++ src/components/Elements/Button/Button.jsx
@@ -1,6 +1,7 @@
-const Button = ({ onSelect, children, classname }) => {
+const Button = ({ onSelect, children, classname, type }) => {
   return (
     <button
+      type={type}
       className={`${classname} px-5 py-2 rounded-lg poppins-regular`}
       onClick={onSelect}
     >
