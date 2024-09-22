02e4d64f481147d9609aa5697f78e337e108923b
myogaazwar <yogaazwar123@gmail.com>
2024-09-22 22:17:05 +0700

    create: context user, login & use component protected route, redirect route

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { createContext, StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './App.css';
@@ -7,6 +7,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import RegisterPage from './pages/Register';
 import LoginPage from './pages/Login';
+import ProfilePage from './pages/Profile';
+import ProtectedRoute from './middleware/protectedRoute';
+import RedirectRoute from './middleware/redirectRoute';
 
 const router = createBrowserRouter([
   {
@@ -15,16 +18,45 @@ const router = createBrowserRouter([
   },
   {
     path: '/register',
-    element: <RegisterPage />,
+    element: (
+      <RedirectRoute>
+        <RegisterPage />
+      </RedirectRoute>
+    ),
   },
   {
     path: '/login',
-    element: <LoginPage />,
+    element: (
+      <RedirectRoute>
+        <LoginPage />
+      </RedirectRoute>
+    ),
+  },
+  {
+    path: '/profile',
+    element: (
+      <ProtectedRoute>
+        <ProfilePage />
+      </ProtectedRoute>
+    ),
   },
 ]);
 
+export const userContext = createContext();
+export const loginContext = createContext();
+
+const App = () => {
+  const [user, setUser] = useState();
+
+  return (
+    <userContext.Provider value={{ user, setUser }}>
+      <RouterProvider router={router} />
+    </userContext.Provider>
+  );
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
