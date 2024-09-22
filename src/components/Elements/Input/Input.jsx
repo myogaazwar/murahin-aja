62b13addba2edf7d1d5b0a3cb08072e5802c9b39
myogaazwar <yogaazwar123@gmail.com>
2024-09-22 19:16:35 +0700

    add: props validation

--- src/components/Elements/Input/Input.jsx
+++ src/components/Elements/Input/Input.jsx
@@ -1,10 +1,11 @@
-export const Input = ({ id, name, type, placeholder }) => {
+export const Input = ({ id, name, type, placeholder, validation }) => {
   return (
     <input
       id={id}
       name={name}
       type={type}
       placeholder={placeholder}
+      {...validation}
       className='border outline-none  focus:border-blue-600 placeholder:text-sm text-sm text-slate-500 px-2 py-2'
     />
   );
