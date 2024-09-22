6fcd7bef7a8083aa4ce3a92e1ce51e0016ae777e
myogaazwar <yogaazwar123@gmail.com>
2024-09-22 19:16:20 +0700

    add: props validation

--- src/components/Elements/Input/Index.jsx
+++ src/components/Elements/Input/Index.jsx
@@ -1,11 +1,17 @@
 import { Input } from './Input';
 import { Label } from './Label';
 
-const InputForm = ({ name, title, id, type, placeholder }) => {
+const InputForm = ({ name, title, id, type, placeholder, validation }) => {
   return (
     <div className='flex flex-col mt-5 poppins-regular gap-y-2'>
       <Label htmlFor={name}>{title}</Label>
-      <Input id={id} name={name} type={type} placeholder={placeholder} />
+      <Input
+        validation={validation}
+        id={id}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+      />
     </div>
   );
 };
