73477f19c6b1a792295e7d3e11312775e58a475c
myogaazwar <yogaazwar123@gmail.com>
2024-09-22 22:32:15 +0700

    create: state Success Message when success register and create useEffect for timer message

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,21 @@ import bgRegister from '/image/bg-register.jpg';
 
 import AuthLayouts from '../components/Layouts/AuthLayouts';
 import FormRegister from '../components/FormInputs/FormRegister';
+import { useEffect, useState } from 'react';
 
 const RegisterPage = () => {
+  const [successMessage, setSuccessMessage] = useState();
+
+  useEffect(() => {
+    if (successMessage) {
+      const timer = setTimeout(() => {
+        setSuccessMessage('');
+      }, 8000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [successMessage]);
+
   return (
     <AuthLayouts
       type={'register'}
@@ -11,7 +24,10 @@ const RegisterPage = () => {
       image={bgRegister}
       description={'Join With US'}
     >
-      <FormRegister />
+      <h1 className='text-center mt-5 poppins-bold-italic text-green-500  drop-shadow-sm'>
+        {successMessage}
+      </h1>
+      <FormRegister setSuccessMessage={setSuccessMessage} />
     </AuthLayouts>
   );
 };
